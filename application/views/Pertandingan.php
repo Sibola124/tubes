22029ee2d4b14d9c6b8e879d281cce3a9692625d
faizalakbari <faizalakbariputra@gmail.com>
2019-09-30 15:45:50 +0700

    view pertandingan
    view pertandingan

---
+++ application/views/Pertandingan.php
@@ -0,0 +1,38 @@
+<style type="text/css">
+  .table {
+    border-collapse: separate;
+    border-spacing:0 20px;
+}
+</style>
+<div class="container" style="margin-top: 150px; margin-bottom: 150px;">
+    <div class="row mt-3">
+      <div class="col">
+        <div class="card">
+          <div class="text-center">
+            <h3>Hasil Pertandingan</h3>
+          </div>
+          <div class="card-body">
+            <table class="table">
+              <tbody style="color: black; background-color: #f5f5ef;">
+                <tr>
+                  <td><h5>21 September 2019</h5></td>
+                  <td><h5>Persib VS Persija</h5></td>
+                  <td><button type="button" class="btn btn-info btn-lg float-right">Lihat Hasil</button></td>
+                </tr>
+                <tr>
+                  <td><h5>21 September 2019</h5></td>
+                  <td><h5>Persib VS Persija</h5></td>
+                  <td><button type="button" class="btn btn-info btn-lg float-right">Lihat Hasil</button></td>
+                </tr>
+                <tr>
+                  <td><h5>21 September 2019</h5></td>
+                  <td><h5>Persib VS Persija</h5></td>
+                  <td><button type="button" class="btn btn-info btn-lg float-right">Lihat Hasil</button></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
