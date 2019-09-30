67b4590e7fa785e6ffcfffa7ca126e0cd14127b8
Adlan <siribeam@gmail.com>
2019-09-30 23:02:21 +0700

    Update Jadwal.php

---
+++ application/views/Jadwal.php
@@ -0,0 +1,41 @@
+<style type="text/css">
+  .table {
+    border-collapse: separate;
+    border-spacing:0 20px;
+}
+</style>
+<div class="container" style="margin-top: 150px; margin-bottom: 150px;">
+    <div class="row mt-3">
+      <div class="col">
+        <div class="card">
+          <div class="text-center">
+            <h3>Jadwal Pertandingan</h3>
+          </div>
+          <div class="card-body">
+            <table class="table">
+            <thead style="background-color: #262626;">
+                <tr style="color: white;">
+                  <th scope="col">Tanggal</th>
+                  <th scope="col">Tim yang bertanding</th>              
+                </tr>
+              </thead>
+              <tbody style="color: black; background-color: #f5f5ef;">
+                <tr>
+                  <td><h5>5 Oktober 2019</h5></td>
+                  <td><h5>Persibanten VS Persib</h5></td>
+                </tr>
+                <tr>
+                  <td><h5>8 Oktober 2019</h5></td>
+                  <td><h5>Bojong Pride VS MU</h5></td>
+                </tr>
+                <tr>
+                  <td><h5>12 Oktober 2019</h5></td>
+                  <td><h5>Persib VS Bojong Pride</h5></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
