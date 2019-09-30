8fd68bf0dd11223d5d23200dcb30479293451cf7
Adlan <siribeam@gmail.com>
2019-09-30 22:52:29 +0700

    tim page

--- application/controllers/Tim.php
+++ application/controllers/Tim.php
@@ -1,12 +1,19 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 class Tim extends CI_Controller {
+	public function __construct()
+    {
+        parent::__construct();
+            $this->load->model("Tim_model");
+    }
+
     public function index($error = NULL) {
-    	$data = array(
-            'judul' => 'Tim',
-            'error' => $error
-        );
+        $data['judul'] = 'Tim';
+    	$data['tim'] = $this->Tim_model->getAll();
+    	$data['pos'] = 1;
         $this->load->view('templates/Navbar', $data);
-        $this->load->view('Tim',$data);
+        $this->load->view('Tim', $data);
         $this->load->view('templates/footer');
+        
+        
     }
 }
---
+++ application/views/Tim.php
@@ -0,0 +1,31 @@
+
+
+<div class="container" style="margin-top: 150px; margin-bottom: 150px;">
+    <div class="row mt-3">
+      <div class="col">
+        <div class="card">
+          <div class="text-center">
+            <h3>Tim</h3>
+          </div>
+          <div class="card-body">
+            <table class="table">
+              <thead style="background-color: #262626;">
+              </thead>
+              <tbody style="color: black; background-color: #f2f2f2;">
+                <?php 
+                $i = 1;
+                foreach ($tim as $t): ?>
+                  <tr>
+                    <td><center><img src="<?php echo base_url('images/'.$t->img)?>" style="width:40px; height:40px;"></center></td>
+                    <td><?php echo $t->nama ?></td>                   
+                </tr>
+                
+                <?php endforeach ?>
+                
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
